491e61de1cd445010cb7f3a041fb0657d52b3754
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-14 17:37:46 +0200

    Mongo DB

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 const express = require ('express');
 const server = express();
+const config = require ('./config/dev');
+
+
+const mongoose = require('mongoose');
+mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Connected to DB!');
+    }
+    
+})
 
 server.use('/api/v1/portfolios/', require('./routes/portfolios'));
 
