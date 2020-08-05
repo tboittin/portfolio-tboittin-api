cc3c39d12e608f0a1aee3b73437fa114e0f98543
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-05 17:12:43 +0200

    152. Create blog on server

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,7 +18,20 @@ exports.getBlogById = async (req, res) => {
     return res.json(blogs)
 }
 
-exports.getBlogById = async (req, res) => {
+exports.getBlogBySlug = async (req, res) => {
     const blogs = await Blog.findOne({slug: req.params.slug});
     return res.json(blogs)
+}
+
+exports.createBlog = async (req, res) => {
+    const blogData = req.body;
+    blogData.userId = req.user.sub;
+    const blog = new Blog(blogData);
+
+    try {
+        const createdBlog = await blog.save();
+        return res.json(createdBlog);
+    } catch(e) {
+        return res.status(422).send(e);
+    }
 }
