1f7b3d1cf419e7bacdedb4945179521e83050cb3
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-24 15:02:16 +0200

    html page

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ async function runServer () {
     server.use('/api/v1/portfolios', require('./routes/portfolios'));
     server.use('/api/v1/blogs', require('./routes/blogs'));
     
+    server.get('/test', (req, res) => {
+        res.json({message: 'Hello World!'});
+    })
+    
+    server.get('', (req, res) => {
+        res.sendFile('index.html', {root: __dirname});
+    })
+
     const PORT = parseInt(process.env.PORT,10) || 3001;
     server.listen(PORT, (err) => {
         if (err) console.error(err);
